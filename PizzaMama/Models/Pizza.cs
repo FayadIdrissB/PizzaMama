681b30301a24db5dbcca521a985e5024f437ff00
FayadIdrissB <fayad-idriss.bacari@outlook.com>
2024-12-25 15:24:10 +0100

    add dependancie and pizza classes

---
+++ PizzaMama/Models/Pizza.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PizzaMama.Models
+{
+	public class Pizza
+	{
+		public int PizzaId { get; set; }
+		public string Nom { get; set; }
+		public float Prix { get; set; }
+		public bool Vegetarien { get; set; }
+		public string Ingredient { get; set; }
+	}
+}
+
