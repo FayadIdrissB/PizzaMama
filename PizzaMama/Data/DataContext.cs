3d5800383c77582dcc0fb69c9085cbccfc119f7a
FayadIdrissB <fayad-idriss.bacari@outlook.com>
2024-12-25 19:29:51 +0100

    add DataContext + ConnectionStrings

---
+++ PizzaMama/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using PizzaMama.Models;
+
+namespace PizzaMama.Data
+{
+    public class DataContext : DbContext
+    {
+        //DbSet correspond à une collection de "Pizza" 
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
+
